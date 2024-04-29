b52a2ea1490c41a5a923a0eef9d05c1210c50f49
Choiseeun0815 <salmons11@naver.com>
2024-04-29 19:33:02 +0900

    Update_GameManager
    초기 아이템 리스트 업데이트

--- GameManager.cs
+++ GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Numerics;
 using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
@@ -7,6 +8,7 @@ namespace Sparta2ndTeam_TeamProject
 {
     internal class GameManager
     {
+        static public List<Item> items = new List<Item>();
         public GameManager()
         {
             InitializeGame();
@@ -26,12 +28,21 @@ namespace Sparta2ndTeam_TeamProject
             }
         }
 
-
-        static Player player;
+        static public Player player;
         private void InitializeGame()
         {
             // 스타트 하면서 생성할 클래스 모음 - 아이템, 캐릭터
+            items.Add(new Item("수련자 갑옷", "수련에 도움을 주는 갑옷입니다.", 0, 5, 0, 1000, ItemType.ARMOR,false,true));
+            items.Add(new Item("무쇠 갑옷", "무쇠로 만들어져 튼튼한 갑옷입니다.", 0, 9, 0, 200, ItemType.ARMOR));
+            items.Add(new Item("스파르타의 갑옷", "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 0, 15, 0, 3500, ItemType.ARMOR));
+            items.Add(new Item("저주받은 갑옷", "그 어떠한 공격도 막아낼 수 있으나...", -30, 40, 0, 8000, ItemType.ARMOR));
 
+            items.Add(new Item("낡은 검", "쉽게 볼 수 있는 낡은 검입니다.", 2, 0, 0, 600, ItemType.WEAPON));
+            items.Add(new Item("청동 도끼", "어디선가 사용됐던거 같은 도끼입니다.", 5, 0, 0, 1500, ItemType.WEAPON,false,true));
+            items.Add(new Item("스파르타의 창", "스파르타의 전사들이 사용했다는 전설의 창입니다.", 7, 0, 0, 2000, ItemType.WEAPON));
+            items.Add(new Item("타노스의 건틀렛", "큰 힘에는 큰 책임이 따릅니다.", 20, -10, 0, 5000, ItemType.WEAPON));
+
+            items.Add(new Item("HP 포션", "HP를 30만큼 회복합니다.", 0, 0, 30, 50, ItemType.PORTION));
 
         }
         public void SaveData()
@@ -150,9 +161,11 @@ namespace Sparta2ndTeam_TeamProject
                     // 스태이터스 메뉴 이동
                     break;
                 case SelectMainMenu.InventoryMenu:
+                    Inventory.InventoryMenu();
                     // 인벤토리 메뉴 이동
                     break;
                 case SelectMainMenu.StoreMenu:
+                    Store.StoreMenu();
                     // 스토어 메뉴 이동
                     break;
             }
