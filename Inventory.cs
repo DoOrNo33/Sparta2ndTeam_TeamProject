417d86772c0412c7791713823b3359a41fee2bc7
Choiseeun0815 <salmons11@naver.com>
2024-04-30 09:48:20 +0900

    Update_Inventory
    똑같은 항목의 아이템 동시 착용 불가능에 대한 기능 구현

--- Inventory.cs
+++ Inventory.cs
@@ -4,7 +4,14 @@ namespace Sparta2ndTeam_TeamProject
 {
     internal class Inventory
     {
+        static int command; 
+
         static bool isFirst = true;
+
+        //현재 무기/방어구가 착용 중인지 아닌지에 대한 정보를 저장하는 변수
+        static bool isEquipedWeapon = false;
+        static bool isEquipedArmor = false;
+
         static List<Item> invenItems = new List<Item>();
         internal static void InventoryMenu()
         {
@@ -12,7 +19,7 @@ namespace Sparta2ndTeam_TeamProject
             {
                 Console.Clear();
 
-                ConsoleUtility.ShowTitle("< 인벤토리 >");
+                ConsoleUtility.ShowTitle("■ 인벤토리 ■");
                 Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
 
                 ConsoleUtility.PrintTextHighlights("", "[아이템 목록]");
@@ -39,7 +46,15 @@ namespace Sparta2ndTeam_TeamProject
                 
                 Console.WriteLine("\n\n1. 장착 관리\n0. 나가기\n");
 
-                int command = ConsoleUtility.PromptMenuChoice(0, 1);
+                if (command == (int)SelectInventoryMenu.WrongCommand)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("잘못된 입력입니다.");
+                    Console.ResetColor();
+                    Console.WriteLine();
+                }
+
+                command = ConsoleUtility.PromptMenuChoice(0, 1);
 
                 switch (command)
                 {
@@ -49,9 +64,6 @@ namespace Sparta2ndTeam_TeamProject
                         EquipMenu();
                         break;
                     default:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("잘못된 입력입니다.");
-                        Console.ResetColor();
                         break;
                 }
             }
@@ -60,9 +72,10 @@ namespace Sparta2ndTeam_TeamProject
         private static void EquipMenu()
         {
             Console.Clear();
-            ConsoleUtility.ShowTitle("< 인벤토리 - 장착 관리 >");
+            ConsoleUtility.ShowTitle("■ 인벤토리 - 장착 관리 ■");
             Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
 
+            ConsoleUtility.PrintTextHighlights("", "[아이템 목록]");
             for (int i = 0; i < invenItems.Count; i++)
             {
                 invenItems[i].PrintItemStatDesc(true, i+1);
@@ -70,24 +83,50 @@ namespace Sparta2ndTeam_TeamProject
 
             }
 
-            int command = ConsoleUtility.PromptMenuChoice(0, GameManager.items.Count);
+            Console.WriteLine();
+            if (command == (int)SelectInventoryMenu.WrongCommand)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("잘못된 입력입니다.");
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+
+            command = ConsoleUtility.PromptMenuChoice(0, GameManager.items.Count);
 
             switch (command)
             {
                 case (int)SelectInventoryMenu.PreviousPage:
                     return;
                 case (int)SelectInventoryMenu.WrongCommand:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("잘못된 입력입니다.");
-                    Console.ResetColor();
+                    EquipMenu();
                     break;
                 default:
-                    invenItems[command - 1].ToggleEquipStatus();
+                    setEquipItems(command);
                     EquipMenu();
                     break;
             }
         }
 
+        static void setEquipItems(int command)
+        {
+            invenItems[command - 1].ToggleEquipStatus();
+
+            //현재 인벤토리에 있는 아이템 중, 
+            for (int i = 0; i < invenItems.Count; i++)
+            {
+                //현재 착용한 아이템과 타입이 일치하는 아이템이
+                if (i != (command-1) && invenItems[command - 1]._type == invenItems[i]._type)
+                {
+                    //장착 상태라면, 
+                    if (invenItems[i].isEquipped)
+                    {
+                        //장착 상태를 해제
+                        invenItems[i].ToggleEquipStatus();
+                    }
+                }
+            }
+        }
         private enum SelectInventoryMenu
         {
             PreviousPage,
