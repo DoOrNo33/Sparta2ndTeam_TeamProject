09f76fd3471a793b013de47493d131cd697ba06f
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-05-01 10:43:34 +0900

    Fix_InitItemList
    아이템 리스트 공유된 버전으로 수정

--- GameManager.cs
+++ GameManager.cs
@@ -39,17 +39,23 @@ namespace Sparta2ndTeam_TeamProject
         private void InitializeGame()
         {
             // 스타트 하면서 생성할 클래스 모음 - 아이템, 캐릭터
-            items.Add(new Item("수련자 갑옷", "수련에 도움을 주는 갑옷입니다.", 0, 5, 0, 1000, ItemType.ARMOR));
-            items.Add(new Item("무쇠 갑옷", "무쇠로 만들어져 튼튼한 갑옷입니다.", 0, 9, 0, 200, ItemType.ARMOR));
-            items.Add(new Item("스파르타의 갑옷", "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 0, 15, 0, 3500, ItemType.ARMOR));
-            items.Add(new Item("저주받은 갑옷", "그 어떠한 공격도 막아낼 수 있으나...", -30, 40, 0, 8000, ItemType.ARMOR));
+            // 방어구 인덱스 0~3                                       공격 | 방어 | 체력 | 마나 | 판매 가격 | 아이템 타입
+            items.Add(new Item("수련자 갑옷", "수련에 도움을 주는 갑옷입니다.", 0, 5, 0, 0, 1000, ItemType.ARMOR));
+            items.Add(new Item("무쇠 갑옷", "무쇠로 만들어져 튼튼한 갑옷입니다.", 0, 9, 0, 0, 200, ItemType.ARMOR));
+            items.Add(new Item("스파르타의 갑옷", "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 0, 15, 0, 0, 3500, ItemType.ARMOR));
+            items.Add(new Item("저주받은 갑옷", "그 어떠한 공격도 막아낼 수 있으나...", -30, 40, 0, 0, 8000, ItemType.ARMOR));
+            // 무기 인덱스 4~7
+            items.Add(new Item("낡은 검", "쉽게 볼 수 있는 낡은 검입니다.", 2, 0, 0, 0, 600, ItemType.WEAPON));
+            items.Add(new Item("청동 도끼", "어디선가 사용됐던거 같은 도끼입니다.", 5, 0, 0, 0, 1500, ItemType.WEAPON));
+            items.Add(new Item("스파르타의 창", "스파르타의 전사들이 사용했다는 전설의 창입니다.", 7, 0, 0, 0, 2000, ItemType.WEAPON));
+            items.Add(new Item("타노스의 건틀렛", "큰 힘에는 큰 책임이 따릅니다.", 20, -10, 0, 0, 5000, ItemType.WEAPON));
+            // 포션 인덱스 8~9                  공격 | 방어 | 체력 | 마나 | 판매 가격 | 아이템 타입 | 착용 여부 | 판매 여부 | 초기 지급 아이템 여부
+            items.Add(new Item("소형 HP 포션", "HP를 30만큼 회복합니다.", 0, 0, 30, 0, 50, ItemType.PORTION, false, false, true));
+            items.Add(new Item("대형 HP 포션", "HP를 70만큼 회복합니다.", 0, 0, 70, 0, 100, ItemType.PORTION));
 
-            items.Add(new Item("낡은 검", "쉽게 볼 수 있는 낡은 검입니다.", 2, 0, 0, 600, ItemType.WEAPON));
-            items.Add(new Item("청동 도끼", "어디선가 사용됐던거 같은 도끼입니다.", 5, 0, 0, 1500, ItemType.WEAPON));
-            items.Add(new Item("스파르타의 창", "스파르타의 전사들이 사용했다는 전설의 창입니다.", 7, 0, 0, 2000, ItemType.WEAPON));
-            items.Add(new Item("타노스의 건틀렛", "큰 힘에는 큰 책임이 따릅니다.", 20, -10, 0, 5000, ItemType.WEAPON));
-
-            items.Add(new Item("HP 포션", "HP를 30만큼 회복합니다.", 0, 0, 30, 50, ItemType.PORTION));
+            // 몬스터 드랍 아이템 인덱스 10~ 
+            items.Add(new Item("작은 혈석", "몬스터에게서 떨어져 나온 의문의 혈석입니다.(마나 15만큼 회복)", 0, 0, 0, 15, 1500, ItemType.MONSTER_DROP));
+            items.Add(new Item("거대한 혈석", "몬스터에게서 떨어져 나온 의문의 혈석입니다.(마나 30만큼 회복)", 0, 0, 0, 30, 3000, ItemType.MONSTER_DROP));
 
         }
         public void SaveData()
