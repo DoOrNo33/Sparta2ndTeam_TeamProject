12725957d471992301cbeb687efdd24b7bfb2f7d
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-05-02 09:48:09 +0900

    Feature_Pet

---
+++ Items/Pet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta2ndTeam_TeamProject.Items
+{
+    internal class Pet : Item
+    {
+        public Pet(string Name, string Desc, int Atk, int Def, int HP, int MP, int Price,
+    ItemType _type, bool isEquipped = false, bool isPurchased = false, bool isInitItem = false) : base(Name, Desc, Atk, Def, HP, MP, Price, _type, isEquipped, isPurchased, isInitItem)
+        {
+            this.Name = Name; 
+            this.Desc = Desc; 
+            this._type = _type;
+            this.isEquipped = isEquipped; 
+            this.isPurchased = isPurchased; 
+            this.isInitItem = isInitItem;
+
+        }
+
+        public virtual void PetSkill()
+        {
+
+        }
+    }
+}
