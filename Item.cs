03bf18d47bed8a41cd71262891b20c257fc00a54
Choiseeun0815 <salmons11@naver.com>
2024-05-03 14:36:45 +0900

    Fix_Item
    펫의 경우 공격력과 방어력에 대한 콘솔 출력 X

--- Item.cs
+++ Item.cs
@@ -55,28 +55,33 @@ namespace Sparta2ndTeam_TeamProject
             }
             else Console.Write(ConsoleUtility.PadRightForMixedText(Name, 20));
 
-            Console.Write(" | ");
+            if (this._type != ItemType.Pet)
+            {
+                Console.Write(" | ");
 
             _MarkAtk = Atk > 0 ? "+" : "";
             _MarkDef = Def > 0 ? "+" : "";
 
-            //아이템이 공격력과 방어력 모두에게 영향을 미치는 경우,
-            if (Atk != 0 && Def != 0)
-            {
-                Console.Write(ConsoleUtility.PadRightForMixedText(($"공격력 {_MarkAtk}{Atk}, 방어력 {_MarkDef}{Def}"), 22));
-            }
-            else
-            {
-                if (Atk != 0)
-                    Console.Write(ConsoleUtility.PadRightForMixedText(($"공격력 {_MarkAtk}{Atk}"), 22));
-                if (Def != 0)
-                    Console.Write(ConsoleUtility.PadRightForMixedText(($"방어력 {_MarkDef}{Def}"), 22));
-                if (HP != 0)
-                    Console.Write(ConsoleUtility.PadRightForMixedText(($"체  력 +{HP}"), 22));
-                if (MP != 0)
-                    Console.Write(ConsoleUtility.PadRightForMixedText(($"마  나 +{MP}"), 22));
+            
+                //아이템이 공격력과 방어력 모두에게 영향을 미치는 경우,
+                if (Atk != 0 && Def != 0)
+                {
+                    Console.Write(ConsoleUtility.PadRightForMixedText(($"공격력 {_MarkAtk}{Atk}, 방어력 {_MarkDef}{Def}"), 22));
+                }
+                else
+                {
+                    if (Atk != 0)
+                        Console.Write(ConsoleUtility.PadRightForMixedText(($"공격력 {_MarkAtk}{Atk}"), 22));
+                    if (Def != 0)
+                        Console.Write(ConsoleUtility.PadRightForMixedText(($"방어력 {_MarkDef}{Def}"), 22));
+                    if (HP != 0)
+                        Console.Write(ConsoleUtility.PadRightForMixedText(($"체  력 +{HP}"), 22));
+                    if (MP != 0)
+                        Console.Write(ConsoleUtility.PadRightForMixedText(($"마  나 +{MP}"), 22));
 
+                }
             }
+            
 
             Console.Write(" | ");
             Console.Write(ConsoleUtility.PadRightForMixedText(Desc, 57));
